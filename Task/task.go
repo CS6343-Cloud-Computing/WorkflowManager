d59645677e9aefdf8b887ea613d9c9aad67026ec
sjchoure <sourabchoure98@gmail.com>
2022-10-01 14:07:35 -0500

    Base 3

--- Task/task.go
+++ Task/task.go
@@ -36,3 +36,71 @@ type TaskEvent struct {
 	Timestamp time.Time
 	Task Task
 }
+
+type Config struct{
+	Name string
+	AttachStdin bool
+	AttachStdout bool
+	AttachStderr bool
+	Cmd []string
+	Image string
+	Memory int64
+	Disk int64
+	Env []string
+	RestartPolicy string
+}
+
+type Docker struct{
+	Client *client.Client
+	Config Config
+	ContainerId string
+}
+
+type DockerResult struct{
+	Error error
+	Action string
+	ContainerId string
+	Result string
+}
+
+func (d *Docker) Run() DockerResult{
+	ctx := context.Backgroud()
+	reader,err := d.Client.ImagePull(ctx, d.Config.Image, types.ImagePullOptions{})
+	if err != nil {
+		log.Printf("Error pulling the image %s: %v \n", d.Config.Image, err)
+		return DockerResult(Error: err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+			Name: d.Config.RestartPolicy,
+	}
+	
+	r := container.Resources{
+			Memory: d.Config.Memory,
+	}
+
+	cc := container.Config{
+			Image: d.Config.Image,
+			Env: d.Config.Env,
+	}
+
+	hc := container.HostConfig{
+			RestartPolicy: rp,
+			Resources: r,
+			PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v \n", d.Config.Image, err)
+		return DockerResult{Error:err}
+	}
+
+	err := d.Client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Printf("Error starting container using image %s: %v \n", resp.ID, err)
+		return DockerResult{Error:err}
+	}
+}
+
