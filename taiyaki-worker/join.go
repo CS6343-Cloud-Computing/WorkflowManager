8dafcc0abdc20f327da5641be65a226922637da5
sjchoure <sourabhchoure98@gmail.com>
2022-10-09 20:18:38 +0000

    Worker Node

--- taiyaki-worker/join.go
+++ taiyaki-worker/join.go
@@ -7,12 +7,11 @@ import (
 )
 
 func NodeJoin(workerIP string, workerPort string, serverIP string, serverPort string, joinKey string) string {
-	fmt.Println("Starting Join Process...")
+	fmt.Println("Joining the cluster...")
 
 	reqJoin :=  NodeJoinReq{NodeIP: workerIP, NodePort: workerPort, JoinKey: joinKey}
 	reqBuffer := new(bytes.Buffer)
 	json.NewEncoder(reqBuffer).Encode(reqJoin)
-	fmt.Println("Sending initial join request...")
 
 	resp, err := reqServer("http://"+serverIP+":8080/node/join", reqBuffer)
 	if err != nil {
@@ -21,24 +20,7 @@ func NodeJoin(workerIP string, workerPort string, serverIP string, serverPort st
 
 	fmt.Println(string(resp))
 
-	// type NodeInitialJoinResp struct {
-	// 	CaCert     string
-	// 	ClientCert string
-	// 	Success    bool   `json:"success"`
-	// 	Error      string `json:"error"`
-	// }
-
-	// respj := NodeInitialJoinResp{}
-	// err = json.Unmarshal(resp, &respj)
-	// if err != nil {
-	// 	fmt.Println("error:", err)
-	// }
-
-	// fmt.Println("Received response: ", respj)
-
-	// //Step 3
-	// if !respj.Success {
-	// 	panic(respj.Error)
-	// }
+	// Complete the error handling part here
+	fmt.Println("Joined the cluster")
 	return ""
 }
