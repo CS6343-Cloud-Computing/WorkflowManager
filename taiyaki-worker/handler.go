d45172ca094c943facaed8db3af8b469e3efd8ce
sjchoure <sourabhchoure98@gmail.com>
2022-10-30 12:47:17 -0500

    heartbeat changes:

--- taiyaki-worker/handler.go
+++ taiyaki-worker/handler.go
@@ -69,7 +69,7 @@ func (a *Api) GetStatsHandler(w http.ResponseWriter, r *http.Request) {
 func (a *Api) GetHeartbeat(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-
+	fmt.Println("Gettting heartbeat")
 	resp := Resp{Result: "Server is running", Success: true, Error: ""}
 	json.NewEncoder(w).Encode(resp)
 }
