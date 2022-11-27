917adc97e317d11120886e1ae275b295dd465303
m1-vm2 <cs6343g7>
2022-11-27 21:22:03 +0000

    lcds

--- taiyaki-cli/step3.sh
+++ taiyaki-cli/step3.sh
@@ -4,5 +4,5 @@ a=0
 input="./out1.txt"
 while IFS= read -r line
 do
-	docker run -d --name $line --env kafkaserver=10.176.128.170:9090 --env workflow=$line --env limit=200 cds
+	docker run -d --name $line --env kafkaserver=10.176.128.170:9090 --env workflow=$line --env limit=200 lcds
 done < "$input"
