ce8301fd129131442b979f8da77bde81ac5d986c
cs6343g7 <cs6343g7@gmail.com>
2022-11-27 23:44:37 +0000

    Cleaning docker

---
+++ cleanDocker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Killing docker container"
+sudo docker kill $(docker ps -q)
+echo "Running Docker Container Prune"
+sudo docker container prune -f
+echo "Removing the images"
+sudo docker rmi -f $(docker images -aq)
