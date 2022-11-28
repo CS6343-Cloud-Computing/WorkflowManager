ce65e51753a991ad34ad000d294634121dc17e6f
cs6343g7 <cs6343g7@gmail.com>
2022-11-28 00:04:41 +0000

    remove shit

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -5,5 +5,3 @@ echo "Running Docker Container Prune"
 sudo docker container prune -f
 echo "Removing the images"
 sudo docker rmi -f $(docker images -aq)
-====== The End ======
-====== The End ======
